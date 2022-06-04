8063adb8632137c6b76159f8e88aa9e5fdcc0b80
wibox <francesco.info.pagano@gmail.com>
2022-06-04 07:54:37 -0700

    ex8

---
+++ MapReduceHadoop/ex8/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
@@ -0,0 +1,26 @@
+package it.polito.bigdata.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+/**
+ * Basic MapReduce Project - Mapper
+ */
+class MapperBigData extends Mapper<
+                    LongWritable, // Input key type
+                    Text,         // Input value type
+                    Text,         // Output key type
+                    IntWritable> {// Output value type
+    
+    protected void map(
+            LongWritable key,   // Input key type
+            Text value,         // Input value type
+            Context context) throws IOException, InterruptedException {
+
+    }
+}
