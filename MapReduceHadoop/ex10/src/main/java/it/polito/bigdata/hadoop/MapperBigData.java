81ec70d3388fb17785ece9294994f776d95766ee
wibox <francesco.info.pagano@gmail.com>
2022-06-04 08:45:45 -0700

    ex10

---
+++ MapReduceHadoop/ex10/src/main/java/it/polito/bigdata/hadoop/MapperBigData.java
@@ -0,0 +1,29 @@
+package it.polito.bigdata.hadoop;
+
+import it.polito.bigdata.hadoop.DriverBigData.MY_COUNTERS;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+/**
+ * Basic MapReduce Project - Mapper
+ */
+class MapperBigData extends Mapper<
+                    LongWritable, // Input key type
+                    Text,         // Input value type
+                    NullWritable,         // Output key type
+                    NullWritable> {// Output value type
+    
+    protected void map(
+            LongWritable key,   // Input key type
+            Text value,         // Input value type
+            Context context) throws IOException, InterruptedException {
+    	
+    	context.getCounter(MY_COUNTERS.TOTAL_RECORDS).increment(1);
+    }
+}
